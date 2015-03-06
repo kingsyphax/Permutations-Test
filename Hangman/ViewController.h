0edd7d94f6886503bad5ad35e9e7b1d276242d16
Nicholas Ward <npward@airbears2-10-142-143-107.AirBears2.1918.Berkeley.EDU>
2015-03-05 19:13:34 -0800

    Initial Commit

---
+++ Hangman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hangman
+//
+//  Created by Nicholas Ward on 3/5/15.
+//  Copyright (c) 2015 Pineapple Bears. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hangman/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hangman
+//
+//  Created by Nicholas Ward on 3/5/15.
+//  Copyright (c) 2015 Pineapple Bears. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
